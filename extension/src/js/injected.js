f86da789e9383c0040ba88eaa0bf56fb62d6d8b1
0xturboblitz <florent.tavernier@gmail.com>
2022-09-16 23:33:59 +0200

    webpack chrome extension boilerplate that can call npm module in injected script yupeee

---
+++ extension/src/js/injected.js
@@ -0,0 +1,6 @@
+console.log('injected.bundle.js')
+
+const {RelayProvider} = require('@opengsn/provider')
+console.log('RelayProvider', RelayProvider)
+
+console.log('window.ethereum', window.ethereum)
