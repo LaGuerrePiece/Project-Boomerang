59215163a89b3c68123ca4f138662cb66d0d0a7f
Stormower <nicolasletimon@gmail.com>
2022-09-17 02:15:03 +0200

    feat fuji contracts address

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -2,7 +2,11 @@ const hre = require("hardhat");
 
 async function main() {
   const Boomerang = await hre.ethers.getContractFactory("Boomerang");
-  const boomerang = await Boomerang.deploy(a, b);
+  const boomerang = await Boomerang.deploy(
+    "0x7A95fA73250dc53556d264522150A940d4C50238", // gsn forwarder
+    "0x7bbcE28e64B3F8b84d876Ab298393c38ad7aac4C", // worrmhole bridge
+    "0xd384D44Dc07c366C18c682704FEbd2bd0a15C143"  // abacus x-chain account router
+    );                                            // all on fuji testnet
 
   await boomerang.deployed();
 
