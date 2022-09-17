dff4879c41fd509d51aae08ea69222feaf470d43
0xturboblitz <florent.tavernier@gmail.com>
2022-09-17 04:55:05 +0200

    dirty push late of injected script parsing tx

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -13,8 +13,10 @@ async function main() {
 
 
   IERC20_ABI = [{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]
-  const wavax = new hre.ethers.Contract("0xd00ae08403B9bbb9124bB305C09058E32C39A48c", IERC20_ABI);
-  await wavax.connect(await hre.ethers.getSigners()[0]).approve(boomerang.address, "10000000000000000000000000", await hre.ethers.getSigners()[0]);
+  const account = (await hre.ethers.getSigners())[0];
+
+  const wavax = new hre.ethers.Contract("0xd00ae08403B9bbb9124bB305C09058E32C39A48c", IERC20_ABI, account);
+  await wavax.approve(boomerang.address, "10000000000000000000000000");
   console.log("Approved boomerang contract to spend our wavax tokens");
 
   await boomerang.boom("0x86c01DD169aE6f3523D1919cc46bc224E733127F", 0, "0x1337", "0xd00ae08403B9bbb9124bB305C09058E32C39A48c", 10000);
