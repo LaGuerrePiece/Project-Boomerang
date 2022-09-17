8e9f6c734c70579d66e56d5cf8da935b75ddc9de
Stormower <nicolasletimon@gmail.com>
2022-09-17 13:48:38 +0200

    use axelar

--- hardhat/contracts/Boomerang.sol
+++ hardhat/contracts/Boomerang.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@opengsn/contracts/src/ERC2771Recipient.sol";
 
-import "./Wormhole/ITokenBridge.sol";
-import "./Wormhole/PortalWrappedToken.sol";
+// import "./Wormhole/ITokenBridge.sol";
+// import "./Wormhole/PortalWrappedToken.sol";
 
-import "./Abacus/IOutbox.sol";
+import "./Axelar/IAxelarGateway.sol";
 
 // import "hardhat/console.sol";
 
@@ -51,11 +51,24 @@ contract Boomerang is ERC2771Recipient {
     // for the bridge
     uint32 nonce = 0;
 
-    // functions to bridge
-    function bridgeToken(address tokenToBridge, uint256 amt, uint16 receipientChainId, bytes32 recipient) public returns (uint64 sequence) {
+    // functions to bridge with wormhole
+    // function bridgeToken(address tokenToBridge, uint256 amt, uint16 receipientChainId, address recipient) public returns (uint64 sequence) {
+    //     approveTokenBridge(tokenToBridge, amt);
+    //     nonce += 1;
+    //     return ITokenBridge(tokenBridge).transferTokens(tokenToBridge, amt, receipientChainId, recipient, 0, nonce);
+
+    // }
+
+    //bridge with Axelar
+    function bridgeToken(address tokenToBridge, uint256 amt, address recipient) public {
         approveTokenBridge(tokenToBridge, amt);
-        nonce += 1;
-        return ITokenBridge(tokenBridge).transferTokens(tokenToBridge, amt, receipientChainId, recipient, 0, nonce);
+        IAxelarGateway(tokenBridge).sendToken(
+            "polygon", // destination chain name
+            toString(abi.encodePacked(recipient)), // some destination wallet address (should be your own)
+            "USDC", // asset symbol
+            1 // amount (in atomic units)
+        );
+        
     }
 
     function approveTokenBridge(address tokenToBridge, uint256 amt) public returns (bool) {
@@ -66,29 +79,28 @@ contract Boomerang is ERC2771Recipient {
 
     // function to be call by the gsn relayer to send bridge + send a cross chain call
     function boom(address to, uint256 value, bytes calldata data, address bridgedToken, uint256 bridgedAmount) public payable{
-        // require(IERC20(bridgedToken).allowance(msg.sender, address(this)) >= bridgedAmount, "Token to bridge not allowed");
-
-        uint16 destChain = (fujiDomain == block.chainid) ? 4 : 6;
+        require(IERC20(bridgedToken).allowance(msg.sender, address(this)) >= bridgedAmount, "Token to bridge not allowed");
+        uint32 destChain = (fujiDomain == block.chainid) ? bscDomain : fujiDomain;
         address senderInterchainAccount = IInterchainAccountRouter(interchainRouter).getInterchainAccount(bscDomain, msg.sender);
 
-        // // Take user's tokens 
-        // IERC20(bridgedToken).transferFrom(msg.sender, address(this), bridgedAmount);
-        // // Bridge tokens
-        // bridgeToken(bridgedToken, bridgedAmount, destChain, bytes32(uint256(uint160(msg.sender)) << 96));
+        // Take user's tokens 
+        IERC20(bridgedToken).transferFrom(msg.sender, address(this), bridgedAmount);
+        // Bridge tokens
+        bridgeToken(bridgedToken, bridgedAmount, msg.sender);
 
         // Send the cross chain transaction
-        Call memory call = Call({
-            to: to,
-            data: data
-        });
-        Call[] memory theCall = new Call[](1);
-        theCall[0] = call;
+        // Call memory call = Call({
+        //     to: to,
+        //     data: data
+        // });
+        // Call[] memory theCall = new Call[](1);
+        // theCall[0] = call;
 
-        IInterchainAccountRouter(interchainRouter).dispatch(
-            // (fujiDomain == uint32(block.chainid)) ? bscDomain : fujiDomain,
-            bscDomain,
-            theCall
-        ); 
+        // IInterchainAccountRouter(interchainRouter).dispatch(
+        //     // destchain,
+        //     bscDomain,
+        //     theCall
+        // ); 
     }
 
     fallback() external payable {
@@ -97,4 +109,17 @@ contract Boomerang is ERC2771Recipient {
     receive() external payable {
     }
 
+
+function toString(bytes memory data) public pure returns(string memory) {
+    bytes memory alphabet = "0123456789abcdef";
+
+    bytes memory str = new bytes(2 + data.length * 2);
+    str[0] = "0";
+    str[1] = "x";
+    for (uint i = 0; i < data.length; i++) {
+        str[2+i*2] = alphabet[uint(uint8(data[i] >> 4))];
+        str[3+i*2] = alphabet[uint(uint8(data[i] & 0x0f))];
+    }
+    return string(str);
+}
 }
--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -4,20 +4,21 @@ async function main() {
   const Boomerang = await hre.ethers.getContractFactory("Boomerang");
   const boomerang = await Boomerang.deploy(
     "0x7A95fA73250dc53556d264522150A940d4C50238", // gsn forwarder
-    "0x61E44E506Ca5659E6c0bba9b678586fA2d729756", // worrmhole bridge
+    "0xC249632c2D40b9001FE907806902f63038B737Ab", // axelar gateway bridge
     "0xd384D44Dc07c366C18c682704FEbd2bd0a15C143"  // abacus x-chain account router
     );                                            // all on fuji testnetoyu
 
   await boomerang.deployed();
   console.log(`Boomerang contract deployed to ${boomerang.address}`);
 
+  // aUSDC on fuji 0x57F1c63497AEe0bE305B8852b354CEc793da43bB
 
   IERC20_ABI = [{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]
-  const wavax = new hre.ethers.Contract("0xd00ae08403B9bbb9124bB305C09058E32C39A48c", IERC20_ABI, (await hre.ethers.getSigners())[0]);
-  await wavax.approve(boomerang.address, 100000000);
+  const wavax = new hre.ethers.Contract("0x57F1c63497AEe0bE305B8852b354CEc793da43bB", IERC20_ABI, (await hre.ethers.getSigners())[0]);
+  await wavax.approve(boomerang.address, "1000000000000000000");
   console.log("Approved boomerang contract to spend our wavax tokens");
 
-  await boomerang.boom("0x86c01DD169aE6f3523D1919cc46bc224E733127F", 0, "0x00", "0xd00ae08403B9bbb9124bB305C09058E32C39A48c", 100000000);
+  await boomerang.boom("0x86c01DD169aE6f3523D1919cc46bc224E733127F", 0, "0x00", "0x57F1c63497AEe0bE305B8852b354CEc793da43bB", 100000000);
   console.log("Bridged");
 
 }
