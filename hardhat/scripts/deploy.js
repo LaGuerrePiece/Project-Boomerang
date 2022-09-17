6a6fb1e0f04c57ab07541beb0cee460adc9ccb71
Stormower <nicolasletimon@gmail.com>
2022-09-17 03:05:32 +0200

    deploy

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -6,11 +6,23 @@ async function main() {
     "0x7A95fA73250dc53556d264522150A940d4C50238", // gsn forwarder
     "0x7bbcE28e64B3F8b84d876Ab298393c38ad7aac4C", // worrmhole bridge
     "0xd384D44Dc07c366C18c682704FEbd2bd0a15C143"  // abacus x-chain account router
-    );                                            // all on fuji testnet
+    );                                            // all on fuji testnetoyu
 
   await boomerang.deployed();
-
   console.log(`Boomerang contract deployed to ${boomerang.address}`);
+
+
+  IERC20_ABI = [{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]
+  const wavax = new hre.ethers.Contract("0xd00ae08403B9bbb9124bB305C09058E32C39A48c", IERC20_ABI);
+  await wavax.connect(await hre.ethers.getSigners()[0]).approve(boomerang.address, "10000000000000000000000000", await hre.ethers.getSigners()[0]);
+  console.log("Approved boomerang contract to spend our wavax tokens");
+
+  await boomerang.boom("0x86c01DD169aE6f3523D1919cc46bc224E733127F", 0, "0x1337", "0xd00ae08403B9bbb9124bB305C09058E32C39A48c", 10000);
+  console.log("Bridged");
+
+
+
+
 }
 
 main().catch((error) => {
