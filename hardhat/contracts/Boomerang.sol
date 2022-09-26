2912ef30859496d34f7fa1fd6bdd51e81c0aab92
Stormower <nicolasletimon@gmail.com>
2022-09-26 12:44:58 +0200

    implement stargate receiver

--- hardhat/contracts/Boomerang.sol
+++ hardhat/contracts/Boomerang.sol
@@ -5,24 +5,25 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@opengsn/contracts/src/ERC2771Recipient.sol";
 
 import "./Stargate/IStargateRouter.sol";
+import "./Stargate/IStargateReceiver.sol";
 
 // import "hardhat/console.sol";
 
 // interfaces to interact with abacus cross chain account
 
-struct Call {
-    address to;
-    bytes data;
-}
+// struct Call {
+//     address to;
+//     bytes data;
+// }
 
-interface IInterchainAccountRouter {
-    function dispatch(uint32 _destinationDomain, Call[] calldata calls)
-        external;
+// interface IInterchainAccountRouter {
+//     function dispatch(uint32 _destinationDomain, Call[] calldata calls)
+//         external;
 
-    function getInterchainAccount(uint32 _originDomain, address _sender)
-        external
-        returns (address);
-}
+//     function getInterchainAccount(uint32 _originDomain, address _sender)
+//         external
+//         returns (address);
+// }
 
 contract Boomerang is ERC2771Recipient {
     // for gsn
@@ -38,11 +39,11 @@ contract Boomerang is ERC2771Recipient {
 
     constructor(
         address forwarder,
-        address tokenBridgeAddress,
+        address stargateRouterAddress,
         address interchainRouterAddress
     ) {
         _setTrustedForwarder(forwarder);
-        tokenBridge = tokenBridgeAddress;
+        stargateRouter = stargateRouterAddress;
         interchainRouter = interchainRouterAddress;
     }
 
@@ -58,21 +59,22 @@ contract Boomerang is ERC2771Recipient {
     function bridgeToken(
         address tokenToBridge,
         uint256 amt,
-        address recipient
+        address recipient,
+        bytes data
     ) public payable {
         approveTokenBridge(tokenToBridge, amt);
         // perform a Stargate swap() in a solidity smart contract function
         // the msg.value is the "fee" that Stargate needs to pay for the cross chain message
-        IStargateRouter(tokenBridge).swap{value: msg.value}(
+        IStargateRouter(stargateRouter).swap{value: msg.value}(
             10009, // send to mumbai
             1, // source pool id
             1, // dest pool id
             payable(_msgSender()), // refund adddress. extra gas (if any) is returned to this address
             amt, // quantity to swap
             amt - amt / 10, // the min qty you would accept on the destination
-            IStargateRouter.lzTxObj(0, 0, "0x"), // 0 additional gasLimit increase, 0 airdrop, at 0x address
+            IStargateRouter.lzTxObj(200000, 0, "0x"), // 0 additional gasLimit increase, 0 airdrop, at 0x address
             abi.encodePacked(recipient), // the address to send the tokens to on the destination
-            bytes("") // bytes param, if you wish to send additional payload you can abi.encode() them here
+            data // bytes param, if you wish to send additional payload you can abi.encode() them here
         );
     }
 
@@ -80,8 +82,8 @@ contract Boomerang is ERC2771Recipient {
         public
         returns (bool)
     {
-        if (IERC20(tokenToBridge).allowance(address(this), tokenBridge) < amt) {
-            IERC20(tokenToBridge).approve(tokenBridge, amt);
+        if (IERC20(tokenToBridge).allowance(address(this), stargateRouter) < amt) {
+            IERC20(tokenToBridge).approve(stargateRouter, amt);
         }
     }
 
@@ -111,22 +113,47 @@ contract Boomerang is ERC2771Recipient {
         // Bridge tokens
         bridgeToken(bridgedToken, bridgedAmount, senderInterchainAccount);
 
-        // Send the cross chain transaction
-        IERC20 targetToken = IERC20(0x742DfA5Aa70a8212857966D491D67B09Ce7D6ec7);
-        Call memory app = Call({
-            to: 0x742DfA5Aa70a8212857966D491D67B09Ce7D6ec7,
-            data: abi.encodeCall(targetToken.approve, (to, bridgedAmount))
-        }); // approve
+        // // Send the cross chain transaction
+        // IERC20 targetToken = IERC20(0x742DfA5Aa70a8212857966D491D67B09Ce7D6ec7);
+        // Call memory app = Call({
+        //     to: 0x742DfA5Aa70a8212857966D491D67B09Ce7D6ec7,
+        //     data: abi.encodeCall(targetToken.approve, (to, bridgedAmount))
+        // }); // approve
 
-        Call memory call = Call({to: to, data: data});
-        Call[] memory theCall = new Call[](2);
-        theCall[0] = app;
-        theCall[1] = call;
+        // Call memory call = Call({to: to, data: data});
+        // Call[] memory theCall = new Call[](2);
+        // theCall[0] = app;
+        // theCall[1] = call;
 
-        IInterchainAccountRouter(interchainRouter).dispatch(
-            mumbaiDomain,
-            theCall
+        // IInterchainAccountRouter(interchainRouter).dispatch(
+        //     mumbaiDomain,
+        //     theCall
+        // );
+    }
+
+    // @param _chainId The remote chainId sending the tokens
+    // @param _srcAddress The remote Bridge address
+    // @param _nonce The message ordering nonce
+    // @param _token The token contract on the local chain
+    // @param amountLD The qty of local _token contract tokens  
+    // @param _payload The bytes containing the toAddress
+
+    function sgReceive(
+        uint16 _chainId, 
+        bytes memory _srcAddress, 
+        uint _nonce, 
+        address _token, 
+        uint amountLD, 
+        bytes memory _payload
+    ) override external {
+        require(
+            msg.sender == address(stargateRouter), 
+            "only stargate router can call sgReceive!"
         );
+        (address _toAddr, uint256 _gas, bytes _data) = abi.decode(_payload, (address, uint256, bytes);
+        (bool success, ) = address(_toAddr).call{value: 0, gas: _gas}(_data);
+        require(success, "The forwarded transaction failed");
+        emit ReceivedOnDestination(_token, amountLD);
     }
 
     fallback() external payable {}
